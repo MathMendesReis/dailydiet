d37bd7fd980eceaef13b34bc5dd244c27a85e356
Matheus Mendes <matheusmendesreis@outlook.com>
2024-08-06 10:00:58 -0300

    initial commit

---
+++ src/main/java/com/devreis/dailydiet/DailydietApplication.java
@@ -0,0 +1,13 @@
+package com.devreis.dailydiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailydietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailydietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devreis/dailydiet/DailydietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devreis.dailydiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailydietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
