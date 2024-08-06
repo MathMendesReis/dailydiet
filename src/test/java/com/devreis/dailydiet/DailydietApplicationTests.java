49afbdb59fa2ed7172204ec4f61cae73b778d099
Matheus Mendes <matheusmendesreis@outlook.com>
2024-08-06 10:12:25 -0300

    chore: install depedenci junit

--- src/test/java/com/devreis/dailydiet/DailydietApplicationTests.java
+++ src/test/java/com/devreis/dailydiet/DailydietApplicationTests.java
@@ -1,13 +1,19 @@
 package com.devreis.dailydiet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.devreis.dailydiet.util.Calculator;
+
 @SpringBootTest
 class DailydietApplicationTests {
+    private final Calculator calculator = new Calculator();
 
 	@Test
-	void contextLoads() {
-	}
+	 void addition() {
+        assertEquals(2, calculator.add(1, 2));
+    }
 
 }
---
+++ src/test/java/com/devreis/dailydiet/util/Calculator.java
@@ -0,0 +1,9 @@
+package com.devreis.dailydiet.util;
+
+public class Calculator {
+    
+
+    public Integer add(Integer a, Integer b){
+        return a + b;
+    }
+}
